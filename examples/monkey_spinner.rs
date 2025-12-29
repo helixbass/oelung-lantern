0db3d3f2c43b8322c3a8d2b7334bc7997cf8d1c0
Julian Rosse <julian@helixbass.net>
2025-12-29 09:44:42 -0500

    working on spinner

---
+++ examples/monkey_spinner.rs
@@ -0,0 +1,67 @@
+use crossterm::event::{Event, EventStream, KeyCode};
+use tokio::sync::mpsc::channel;
+use tokio_stream::StreamExt;
+
+use oelung::{soft, Renderer};
+
+use oelung_lantern::{generate_sender, mpsc::Sender, spinner::monkey, ReceiveEvent, MonkeySpinner};
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let mut renderer = Renderer::try_new()?;
+
+    let (sender, mut receiver) = channel::<World>(100);
+
+    listen_to_crossterm_events(CrosstermSender::from(sender.clone()));
+
+    let mut spinner = MonkeySpinner::new(None, Box::new(MonkeySpinnerTickSender::from(sender)));
+
+    render_screen(&mut renderer, &spinner)?;
+
+    while let Some(world) = receiver.recv().await {
+        match world {
+            World::Crossterm(Event::Key(key)) if key.code == KeyCode::Char('q') => {
+                break;
+            }
+            World::MonkeySpinnerTick(tick) => {
+                spinner.receive(&tick);
+                render_screen(&mut renderer, &spinner)?;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
+
+fn render_screen(renderer: &mut Renderer, spinner: &MonkeySpinner) -> Result<(), anyhow::Error> {
+    renderer.render(soft! {
+      %Text
+        children => [
+          %spinner
+          %Text " (hit q to quit)"
+        ]
+    })?;
+
+    Ok(())
+}
+
+enum World {
+    Crossterm(Event),
+    MonkeySpinnerTick(monkey::Tick),
+}
+
+generate_sender!(World, Crossterm, Event);
+generate_sender!(World, MonkeySpinnerTick, monkey::Tick);
+
+fn listen_to_crossterm_events(sender: CrosstermSender) {
+    tokio::spawn(async move {
+        let mut event_stream = EventStream::new();
+
+        while let Some(Ok(event)) = event_stream.next().await {
+            sender.send(event).await;
+        }
+
+        panic!("kill everything")
+    });
+}
---
+++ src/spinner/monkey.rs
@@ -0,0 +1,69 @@
+use std::sync::LazyLock;
+use std::time::Duration;
+
+use oelung::{anyhow, Component, ComponentInterface, Grid, TextBuilder};
+use tokio::{task::JoinHandle, time::interval};
+use uuid::Uuid;
+
+use crate::{mpsc::Sender, ReceiveEvent};
+
+fn steps() -> &'static [&'static str] {
+    static STEPS: LazyLock<Vec<&'static str>> =
+        LazyLock::new(|| vec!["🙈", "🙉", "🙊", "🐵"]);
+    &*STEPS
+}
+
+pub struct MonkeySpinner {
+    pub join_handle: JoinHandle<()>,
+    pub next_step: usize,
+    pub uuid: Uuid,
+}
+
+impl MonkeySpinner {
+    pub fn new(period: Option<Duration>, sender: Box<dyn Sender<Tick>>) -> Self {
+        let period = period.unwrap_or_else(|| Duration::from_millis(1200));
+        let uuid = Uuid::new_v4();
+        let join_handle = tokio::spawn(async move {
+            let mut interval = interval(period / u32::try_from(steps().len()).unwrap());
+            loop {
+                let _ = interval.tick().await;
+                sender.send(Tick { uuid }).await;
+            }
+        });
+
+        Self {
+            join_handle,
+            next_step: 0,
+            uuid,
+        }
+    }
+}
+
+impl Drop for MonkeySpinner {
+    fn drop(&mut self) {
+        self.join_handle.abort();
+    }
+}
+
+impl<'a> ComponentInterface for &'a MonkeySpinner {
+    fn render<'b>(&self, _grid: Grid) -> Result<Component<'b>, anyhow::Error> {
+        Ok({
+            let text = TextBuilder::default();
+            let text = text.text_child(steps()[self.next_step % steps().len()]);
+            text.build().unwrap().into()
+        })
+    }
+}
+
+impl ReceiveEvent<Tick> for MonkeySpinner {
+    fn receive(&mut self, event: &Tick) {
+        if event.uuid != self.uuid {
+            return;
+        }
+        self.next_step += 1;
+    }
+}
+
+pub struct Tick {
+    pub uuid: Uuid,
+}
