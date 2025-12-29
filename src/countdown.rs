7305a225d6349f7ca5ba3e698913e79940108f6a
Julian Rosse <julian@helixbass.net>
2025-12-29 10:16:29 -0500

    working on spinner

---
+++ src/countdown.rs
@@ -0,0 +1,56 @@
+use std::time::{Duration, Instant};
+
+use oelung::{anyhow, soft, Component, ComponentInterface, Grid};
+use tokio::{task::JoinHandle, time::interval};
+use uuid::Uuid;
+
+use crate::mpsc::Sender;
+
+pub struct Countdown {
+    pub total: Duration,
+    pub started_at: Instant,
+    pub join_handle: JoinHandle<()>,
+    pub uuid: Uuid,
+}
+
+impl Countdown {
+    pub fn new(total: Duration, sender: Box<dyn Sender<Tick>>) -> Self {
+        let uuid = Uuid::new_v4();
+        let join_handle = tokio::spawn(async move {
+            let mut interval = interval(Duration::from_millis(1));
+            loop {
+                let _ = interval.tick().await;
+                sender.send(Tick { uuid }).await;
+            }
+        });
+
+        Self {
+            total,
+            join_handle,
+            started_at: Instant::now(),
+            uuid,
+        }
+    }
+}
+
+impl Drop for Countdown {
+    fn drop(&mut self) {
+        self.join_handle.abort();
+    }
+}
+
+impl<'a> ComponentInterface for &'a Countdown {
+    fn render<'b>(&self, _grid: Grid) -> Result<Component<'b>, anyhow::Error> {
+        let remaining = format!("{:.3}", self.started_at.elapsed().as_millis() as f64);
+        Ok(soft! {
+            %Text children => [
+              %Text remaining
+              %Text "s"
+            ]
+        })
+    }
+}
+
+pub struct Tick {
+    pub uuid: Uuid,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+mod countdown;
 pub mod mpsc;
 mod receive;
 pub mod spinner;
 
 pub use proc_macros::generate_sender;
 
+pub use countdown::Countdown;
 pub use receive::ReceiveEvent;
 pub use spinner::{
     BallsSpinner, BarSpinner, BounceSpinner, EllipsisSpinner, MonkeySpinner, MoonSpinner,
