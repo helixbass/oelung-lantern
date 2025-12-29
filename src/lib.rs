8c99bf9143c79dbcfdf5ed4138afe2903212d19c
Julian Rosse <julian@helixbass.net>
2025-12-29 09:10:16 -0500

    working on spinner

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,4 @@ pub mod spinner;
 pub use proc_macros::generate_sender;
 
 pub use receive::ReceiveEvent;
-pub use spinner::{BarSpinner, BounceSpinner, SnakeSpinner};
+pub use spinner::{BallsSpinner, BarSpinner, BounceSpinner, SnakeSpinner};
--- src/spinner/mod.rs
+++ src/spinner/mod.rs
@@ -1,7 +1,9 @@
+pub mod balls;
 pub mod bar;
 pub mod bounce;
 pub mod snake;
 
+pub use balls::BallsSpinner;
 pub use bar::BarSpinner;
 pub use bounce::BounceSpinner;
 pub use snake::SnakeSpinner;
