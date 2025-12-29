91f8b56cb8f4fe6a042672f65beaad21cc42a6a9
Julian Rosse <julian@helixbass.net>
2025-12-29 12:52:49 -0500

    working on spinner

---
+++ examples/gradient.rs
@@ -0,0 +1,77 @@
+use crossterm::{
+    event::{Event, EventStream, KeyCode},
+    style::Color,
+};
+use tokio::sync::mpsc::channel;
+use tokio_stream::StreamExt;
+
+use oelung::{soft, Renderer};
+
+use oelung_lantern::{generate_sender, mpsc::Sender, Gradient};
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let mut renderer = Renderer::try_new()?;
+
+    let (sender, mut receiver) = channel::<World>(100);
+
+    listen_to_crossterm_events(CrosstermSender::from(sender.clone()));
+
+    let gradient = Gradient::new(
+        Color::Rgb {
+            r: 20,
+            g: 20,
+            b: 45,
+        },
+        Color::Rgb {
+            r: 20,
+            g: 20,
+            b: 245,
+        },
+        1,
+        40,
+    );
+
+    render_screen(&mut renderer, &gradient)?;
+
+    while let Some(world) = receiver.recv().await {
+        match world {
+            World::Crossterm(Event::Key(key)) if key.code == KeyCode::Char('q') => {
+                break;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
+
+fn render_screen(renderer: &mut Renderer, gradient: &Gradient) -> Result<(), anyhow::Error> {
+    renderer.render(soft! {
+      %FlexColumn
+        children => [
+          %gradient
+          %Text "(hit q to quit)"
+        ]
+    })?;
+
+    Ok(())
+}
+
+enum World {
+    Crossterm(Event),
+}
+
+generate_sender!(World, Crossterm, Event);
+
+fn listen_to_crossterm_events(sender: CrosstermSender) {
+    tokio::spawn(async move {
+        let mut event_stream = EventStream::new();
+
+        while let Some(Ok(event)) = event_stream.next().await {
+            sender.send(event).await;
+        }
+
+        panic!("kill everything")
+    });
+}
