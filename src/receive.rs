82304738acad6e655b705fe6a8755653c8d87d13
Julian Rosse <julian@helixbass.net>
2026-01-06 10:34:19 -0500

    working on spinner

--- src/receive.rs
+++ src/receive.rs
@@ -2,10 +2,10 @@ use std::pin::Pin;
 
 use oelung::anyhow;
 
-pub trait ReceiveEvent<TEvent> {
+pub trait ReceiveEvent<TEvent, TReturn = ()> {
     fn receive<TQueueEffect: FnMut(Pin<Box<dyn Future<Output = ()> + Send + 'static>>)>(
         &mut self,
         event: &TEvent,
         _queue_effect: TQueueEffect,
-    ) -> Result<(), anyhow::Error>;
+    ) -> Result<TReturn, anyhow::Error>;
 }
